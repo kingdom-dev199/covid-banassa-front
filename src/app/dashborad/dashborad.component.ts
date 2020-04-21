9317235556b2cfdde007921ac5530f388e2ff7cf
rachid <5achidouahmane@gmail.com>
2020-04-21 14:18:13 +0000

    add dashboard layout

--- src/app/dashborad/dashborad.component.ts
+++ src/app/dashborad/dashborad.component.ts
@@ -1,6 +1,20 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle
+} from "ng-apexcharts";
 import { Component, OnInit } from '@angular/core';
 import { BanassiService } from 'src/services/banassi.service';
 
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
 
 @Component({
   selector: 'app-dashborad',
@@ -8,7 +22,29 @@ import { BanassiService } from 'src/services/banassi.service';
   styleUrls: ['./dashborad.component.css']
 })
 export class DashboradComponent implements OnInit {
+  @ViewChild('chart') chart: ChartComponent;
+  public chartOptions;
 
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "My-series",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "bar"
+      },
+      title: {
+        text: "My First Angular Chart"
+      },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
+      }
+    };
+  }
   private totalCases:number;
   private totalRecovred:number;
   private totalDeaths:number;
