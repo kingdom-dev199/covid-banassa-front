14e1ffcc05ad5517313a00f666d7cc648d1554a0
rachid <5achidouahmane@gmail.com>
2020-04-22 16:55:20 +0000

    add chart

--- src/app/dashborad/dashborad.component.ts
+++ src/app/dashborad/dashborad.component.ts
@@ -4,15 +4,21 @@ import {
   ApexAxisChartSeries,
   ApexChart,
   ApexXAxis,
-  ApexTitleSubtitle
+  ApexDataLabels,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid
 } from "ng-apexcharts";
-import { Component, OnInit } from '@angular/core';
+
 import { BanassiService } from 'src/services/banassi.service';
 
 export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
   xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
   title: ApexTitleSubtitle;
 };
 
@@ -23,62 +29,105 @@ export type ChartOptions = {
 })
 export class DashboradComponent implements OnInit {
   @ViewChild('chart') chart: ChartComponent;
-  private totalCases:number;
-  private totalRecovred:number;
-  private totalDeaths:number;
-  private totalActiveCases:number;
-  private totalNewCasesToday:number;
-  private totalNewDeathsToday:number;
+   totalCases:number;
+   totalRecovred:number;
+   totalDeaths:number;
+   totalActiveCases:number;
+   totalNewCasesToday:number;
+   totalNewDeathsToday:number;
+   dataset=[];
+   dailycases=[];
+   dates=[];
   
   public chartOptions;
 
   constructor(public banassiService:BanassiService) {
+    this.getDailycases();
+    this.getTotaleCases();
     this.chartOptions = {
       series: [
         {
-          name: "My-series",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+          name: "Desktops",
+          data: this.dailycases
         }
       ],
       chart: {
         height: 350,
-        type: "bar"
+        type: "line",
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
       },
       title: {
-        text: "My First Angular Chart"
+        text: "Product Trends by Month",
+        align: "left"
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5
+        }
       },
       xaxis: {
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
+        categories: this.dates
       }
     };
+    console.log(this.chartOptions)
+
   }
 
+  
   ngOnInit(){
-    this.banassiService.getInfoCovid()
-    .subscribe(data => {
-      //الحالات المؤكدة
-      this.totalCases = data.countrydata[0].total_cases;
-      //المتعافون
-      this.totalRecovred=data.countrydata[0].total_recovered;
-      //الوفيات
-      this.totalDeaths=data.countrydata[0].total_deaths;
-      //حالات تتلقى العلاج
-      this.totalActiveCases=data.countrydata[0].total_active_cases;
-        
-      //optionnal if you want to added (not exist in hesspress )
-      // new Casse Today
-      this.totalNewCasesToday = data.countrydata[0].total_new_cases_today;
-      // new Death Today 
-      this.totalNewDeathsToday = data.countrydata[0].total_new_deaths_today;
-
-
-    }),err => {
-      console.log(err);
-    }
+   
   }
 
 
 
   
 
+
+  private getTotaleCases() {
+    this.banassiService.getInfoCovid()
+      .subscribe((data:any)  => {
+        //الحالات المؤكدة
+        this.totalCases = data.countrydata[0].total_cases;
+        //المتعافون
+        this.totalRecovred = data.countrydata[0].total_recovered;
+        //الوفيات
+        this.totalDeaths = data.countrydata[0].total_deaths;
+        //حالات تتلقى العلاج
+        this.totalActiveCases = data.countrydata[0].total_active_cases;
+        //optionnal if you want to added (not exist in hesspress )
+        // new Casse Today
+        this.totalNewCasesToday = data.countrydata[0].total_new_cases_today;
+        // new Death Today 
+        this.totalNewDeathsToday = data.countrydata[0].total_new_deaths_today;
+      }), err => {
+        console.log(err);
+      };
+  }
+  private getDailycases() {
+    this.banassiService.getInfoCovidStats()
+      .subscribe((data: any)=> {
+        console.log(data.timelineitems);
+        this.dailycases = Object.values(data.timelineitems[0]).map((obj: any) => obj.new_daily_cases).splice(0, 7);
+        this.dates = Object.keys(data.timelineitems[0]).splice(0, 7);
+      //   for (var i: number = 0; i < 8;i++){
+      //     this.dataset.push({ data: this.dates[i], label: this.dailycases[i]})
+      //  }
+        console.log(this.dailycases);
+        console.log(this.dates);
+        console.log(this.chartOptions.series.data);
+        console.log(this.chartOptions.xaxis.categories);
+      
+      }), err => {
+        console.log(err);
+      };
+  }
 }
